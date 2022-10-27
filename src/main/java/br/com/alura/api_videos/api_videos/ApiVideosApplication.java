e1e5b38d40044fd869230d710741c3f058dcf707
davidjunior <scuba-potions-0a@icloud.com>
2022-10-27 16:49:30 -0300

    first commit

---
+++ src/main/java/br/com/alura/api_videos/api_videos/ApiVideosApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.api_videos.api_videos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiVideosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiVideosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alura/api_videos/api_videos/ApiVideosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.api_videos.api_videos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiVideosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
